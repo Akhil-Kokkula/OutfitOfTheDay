3a8a916cbc163942bffe0ee2618d19f2877980f9
bui-jeremy <jeremybui2002@gmail.com>
2024-03-30 00:37:52 -0400

    add camera feature to add outfit fragment

--- app/src/main/java/com/example/outfitoftheday/AddOutfitFragment.kt
+++ app/src/main/java/com/example/outfitoftheday/AddOutfitFragment.kt
@@ -1,17 +1,53 @@
 package com.example.outfitoftheday
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.outfitoftheday.R
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
 
 class AddOutfitFragment : Fragment() {
+    private lateinit var imageView: ImageView
+    private lateinit var takePictureLauncher: ActivityResultLauncher<Intent>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Initialize the ActivityResultLauncher
+        takePictureLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                // Retrieve the thumbnail from the result
+                val imageBitmap = result.data?.extras?.get("data") as Bitmap?
+                imageBitmap?.let {
+                    imageView.setImageBitmap(it)
+                } ?: Toast.makeText(requireContext(), "Failed to capture image!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_add_outfit, container, false)
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val view = inflater.inflate(R.layout.fragment_add_outfit, container, false)
+        imageView = view.findViewById(R.id.imageViewCaptured)
+        val button: Button = view.findViewById(R.id.buttonCapture)
+
+        button.setOnClickListener {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            try {
+                takePictureLauncher.launch(takePictureIntent)
+            } catch (e: Exception) {
+                Toast.makeText(requireContext(), "Camera not available.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        return view
     }
 }
