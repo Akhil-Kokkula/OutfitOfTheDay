6690988b4c56277da1a8f7f1348fd8f2b8f6b50f
bui-jeremy <jeremybui2002@gmail.com>
2024-03-29 22:29:22 -0400

    added navigation bar and home,outfit, and wardrobe fragment

--- app/src/main/java/com/example/outfitoftheday/MainActivity.kt
+++ app/src/main/java/com/example/outfitoftheday/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.outfitoftheday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            var selectedFragment: Fragment? = null
+
+            when (item.itemId) {
+                R.id.navigation_home -> selectedFragment = HomeFragment()
+                R.id.navigation_generate_outfit -> selectedFragment = GenerateOutfitFragment()
+                R.id.navigation_wardrobe -> selectedFragment = WardrobeFragment()
+            }
+
+            selectedFragment?.let {
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_container, it).commit()
+            }
+
+            true
+        }
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HomeFragment()).commit()
+        }
     }
 }
